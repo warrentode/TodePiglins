7821f638d6c214d1145ea20f356612e079677a33
toadie-odie <3997423+toadie-odie@users.noreply.github.com>
2023-04-21 14:14:24 -0400

    Piglin Barter Items tag now works with vanilla Piglins

--- src/main/java/net/warrentode/todepiglins/mixin/PiglinMerchantPortalSpawnRuleMixin.java
+++ src/main/java/net/warrentode/todepiglins/mixin/PiglinMerchantPortalSpawnRuleMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(NetherPortalBlock.class)
-public abstract class PiglinMerchantPortalSpawnRule {
+public abstract class PiglinMerchantPortalSpawnRuleMixin {
     Block pBlock = (Block) (Object) this;
 
     @Inject(at = @At("HEAD"), method = "randomTick")
