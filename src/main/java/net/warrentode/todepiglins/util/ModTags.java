496f82a744dc427132e1cf0fdbe08c3cf8f4ef10
toadie-odie <3997423+toadie-odie@users.noreply.github.com>
2023-04-22 17:16:28 -0400

    Refactored tags to Forge tags for better mod compatibility

--- src/main/java/net/warrentode/todepiglins/util/ModTags.java
+++ src/main/java/net/warrentode/todepiglins/util/ModTags.java
@@ -5,17 +5,15 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
-import static net.warrentode.todepiglins.TodePiglins.MODID;
-
 public class ModTags {
 
     public static class Items {
-        public static final TagKey<Item> PIGLIN_BARTER_ITEMS = modItemTag("piglin_barter_items");
-        public static final TagKey<Item> PIGLIN_WANTED_ITEMS = modItemTag("piglin_wanted_items");
+        public static final TagKey<Item> PIGLIN_BARTER_ITEMS = forgeItemTag("piglin_barter_items");
+        public static final TagKey<Item> PIGLIN_WANTED_ITEMS = forgeItemTag("piglin_wanted_items");
     }
 
     @SuppressWarnings("SameParameterValue")
-    private static TagKey<Item> modItemTag(String path) {
-        return TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(MODID, path));
+    private static TagKey<Item> forgeItemTag(String path) {
+        return TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation("forge", path));
     }
 }
